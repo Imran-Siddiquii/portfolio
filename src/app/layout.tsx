e843c4c8b5e8f34376f53a4a39fdfde9dd4d6e2d
Imran-Siddiquii <siddiquiimran132@gmail.com>
2025-11-10 11:00:18 +0530

    chore: using speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 
@@ -43,7 +44,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        {children} <SpeedInsights />
+      </body>
     </html>
   );
 }
